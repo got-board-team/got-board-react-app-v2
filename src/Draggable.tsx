eda7ba15d859f0be92918636b29f20e284b15b00
Rafael Borgonovi Tauil <rafael@tauil.com.br>
2019-11-12 19:41:48 +0100

    comment

--- src/Draggable.tsx
+++ src/Draggable.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useDrop } from 'react-dnd'
 
 const Draggable = (Component: React.FC) => (props: any) => {
-  console.log('props', props);
+  console.log('props', props); // accept and setter method
   return <Component { ...props } />;
 }
 
