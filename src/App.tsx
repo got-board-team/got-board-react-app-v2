e4783c028c841d53d2fb2f9b1337ecdd8aea0f0c
Rafael Borgonovi Tauil <rafael@tauil.com.br>
2019-11-14 00:27:54 +0100

    Adds WarRoom to Dropable

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ import HTML5Backend from 'react-dnd-html5-backend';
 
 import './App.css';
 import WarRoom from './WarRoom';
-import Piece from './Piece';
+import Piece, { PieceProps } from './Piece';
 import Map from "./Map";
+import Dropable from "./Dropable";
 
 const App: React.FC = () => {
   const [warRoomPieces, setWarRoomPieces] = useState([
@@ -16,12 +17,33 @@ const App: React.FC = () => {
     { id: 5, type: "piece", x: 300, y: 100 },
   ]);
 
+  const updatePiecePosition = (item: any, monitor: any) => {
+    const newCoords = monitor.getDifferenceFromInitialOffset();
+    if (newCoords && newCoords.x && newCoords.y) {
+      const currentPiece = warRoomPieces.find(i => i.id === item.id);
+      if (!currentPiece) return; // Not undefined
+      const otherPieces = warRoomPieces.filter(i => i.id !== item.id);
+      const newPiece: PieceProps = {
+        ...currentPiece,
+        x: newCoords.x + currentPiece.x,
+        y: newCoords.y + currentPiece.y,
+      };
+      const final: PieceProps[] = [
+        ...otherPieces,
+        newPiece,
+      ]
+      setWarRoomPieces(final);
+    }
+  }
+
   return (
     <DndProvider backend={HTML5Backend}>
       <div className="app">
-        <WarRoom id={0} type="war-room" x={50} y={50}>
-          {warRoomPieces.map((piece, index) => <Piece key={index} id={piece.id} x={piece.x} y={piece.y} type="piece" />)}
-        </WarRoom>
+        <Dropable accept="piece" dropAction={updatePiecePosition}>
+          <WarRoom id={0} type="war-room" x={50} y={50}>
+            {warRoomPieces.map((piece, index) => <Piece key={index} id={piece.id} x={piece.x} y={piece.y} type="piece" />)}
+          </WarRoom>
+        </Dropable>
         <Map />
       </div>
     </DndProvider>
